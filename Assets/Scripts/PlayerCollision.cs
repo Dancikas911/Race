076d66f9c29a1e33219aa3b72ecabfcd88edeb91
Dancikas911 <anuskevicd14@gmail.com>
2025-01-19 13:16:05 +0200

    aaa
    aa

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;  // Import TMP namespace
 
 public class PlayerCollision : MonoBehaviour
 {
-    public AudioClip explosionSound;   // Sprogimo garsas
+    public AudioClip explosionSound;
+    public Timer timer;  // Reference to Timer script
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Wall"))
+        if (collision.collider.name == "Wall")  // Check by name, no tag needed
         {
-            // Paslėpk žaidėją
             gameObject.SetActive(false);
 
-            // Rodyk pralaimėjimo ekraną
+            // Reset TMP timer
+            if (timer != null)
+            {
+                timer.ResetTimer();  // Reset Timer (TMP)
+            }
+
             Invoke("GameOver", 2f);
         }
     }
 
     void GameOver()
     {
-        // Galima pridėti Game Over logiką (pvz., scenos perkrovimą)
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
     }
-
 }
